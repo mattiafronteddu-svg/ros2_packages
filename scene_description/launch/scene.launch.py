e6ef15d5cb586610448b86b871e0e49308d1a29a
mattia <mattia.fronteddu@mch-tronics.ch>
2026-02-28 17:03:03 +0100

    scene of Automat completed (also with tools and robot)

--- scene_description/launch/scene.launch.py
+++ scene_description/launch/scene.launch.py
@@ -1,66 +1,38 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import FindExecutable
 
 
 def generate_launch_description():
-    # UR args
-    ur_type = LaunchConfiguration("ur_type")
-    name = LaunchConfiguration("name")
-    tf_prefix = LaunchConfiguration("tf_prefix")
-
-    # Pose args
-    robot_x = LaunchConfiguration("robot_x")
-    robot_y = LaunchConfiguration("robot_y")
-    robot_z = LaunchConfiguration("robot_z")
-    robot_roll = LaunchConfiguration("robot_roll")
-    robot_pitch = LaunchConfiguration("robot_pitch")
-    robot_yaw = LaunchConfiguration("robot_yaw")
-
-    table_x = LaunchConfiguration("table_x")
-    table_y = LaunchConfiguration("table_y")
-    table_z = LaunchConfiguration("table_z")
-    table_roll = LaunchConfiguration("table_roll")
-    table_pitch = LaunchConfiguration("table_pitch")
-    table_yaw = LaunchConfiguration("table_yaw")
-
+    # Launch args
     use_gui = LaunchConfiguration("use_gui")
-
-    # RViz config
     rviz_config = LaunchConfiguration("rviz_config")
 
+    # Paths
     scene_xacro = PathJoinSubstitution([
         FindPackageShare("scene_description"),
         "urdf",
-        "scene_desc.xacro"
+        "_scene.xacro"
     ])
 
+    default_rviz_config = PathJoinSubstitution([
+        FindPackageShare("scene_description"),
+        "rviz",
+        "config.rviz"
+    ])
+
+    # robot_description da xacro
     robot_description = Command([
-        "xacro ",
+        FindExecutable(name="xacro"),
+        " ",
         scene_xacro,
-
-        " ur_type:=", ur_type,
-        " name:=", name,
-        " tf_prefix:=", tf_prefix,
-
-        " robot_x:=", robot_x,
-        " robot_y:=", robot_y,
-        " robot_z:=", robot_z,
-        " robot_roll:=", robot_roll,
-        " robot_pitch:=", robot_pitch,
-        " robot_yaw:=", robot_yaw,
-
-        " table_x:=", table_x,
-        " table_y:=", table_y,
-        " table_z:=", table_z,
-        " table_roll:=", table_roll,
-        " table_pitch:=", table_pitch,
-        " table_yaw:=", table_yaw,
     ])
 
+    # Nodes
     rsp = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
@@ -74,7 +46,6 @@ def generate_launch_description():
         executable="joint_state_publisher_gui",
         output="screen",
         condition=IfCondition(use_gui),
-        parameters=[{"robot_description": robot_description}],
     )
 
     jsp = Node(
@@ -82,7 +53,6 @@ def generate_launch_description():
         executable="joint_state_publisher",
         output="screen",
         condition=UnlessCondition(use_gui),
-        parameters=[{"robot_description": robot_description}],
     )
 
     rviz = Node(
@@ -92,35 +62,7 @@ def generate_launch_description():
         arguments=["-d", rviz_config],
     )
 
-    default_rviz_config = PathJoinSubstitution([
-        FindPackageShare("scene_description"),
-        "rviz",
-        "config.rviz"
-    ])
-
     return LaunchDescription([
-        # UR
-        DeclareLaunchArgument("ur_type", default_value="ur10"),
-        DeclareLaunchArgument("name", default_value="ur10"),
-        DeclareLaunchArgument("tf_prefix", default_value=""),
-
-        # Robot pose
-        DeclareLaunchArgument("robot_x", default_value="0.0"),
-        DeclareLaunchArgument("robot_y", default_value="0.0"),
-        DeclareLaunchArgument("robot_z", default_value="0.775"),
-        DeclareLaunchArgument("robot_roll", default_value="0.0"),
-        DeclareLaunchArgument("robot_pitch", default_value="0.0"),
-        DeclareLaunchArgument("robot_yaw", default_value="0.0"),
-
-        # Table pose
-        DeclareLaunchArgument("table_x", default_value="0.0"),
-        DeclareLaunchArgument("table_y", default_value="0.0"),
-        DeclareLaunchArgument("table_z", default_value="0.75"),
-        DeclareLaunchArgument("table_roll", default_value="0.0"),
-        DeclareLaunchArgument("table_pitch", default_value="0.0"),
-        DeclareLaunchArgument("table_yaw", default_value="0.0"),
-
-        # GUI + RViz config
         DeclareLaunchArgument("use_gui", default_value="true"),
         DeclareLaunchArgument("rviz_config", default_value=default_rviz_config),
 
